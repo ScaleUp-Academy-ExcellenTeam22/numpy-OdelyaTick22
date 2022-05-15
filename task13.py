8e64d5677a911a4b36b0887a223ec6bae0a98bb3
OdelyaTick22 <odecohen@g.jct.ac.il>
2022-05-15 22:16:36 +0300

    Adding task14.py

--- task13.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-if __name__ == "__main__":
-    first = np.array([[10], [20], [30]])
-    second = np.array([[40], [50], [60]])
-    new_array = np.dstack((first, second))
-    print(new_array)
